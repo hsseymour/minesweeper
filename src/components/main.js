c25ccdcc54456836c02ed29b6c04a592cab267ee
Harry Seymour <harryso12@hotmail.co.uk>
2023-10-05 19:33:42 +0100

    begin components

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,25 @@
 import { useState } from "react"
+import { CreateDifficulty } from "./difficulty";
+import { CreateBoard } from "./board";
+import { CreateTimer } from "./timer";
 
 export const CreateMain = () => {
-    [gameState, setGameState] = useState(0);
+    [gameState, SetGameState] = useState(0);
 
+    const UpdateGameState = (gameState) => {
+        SetGameState(parseInt(gameState));
+    }
 
     return (
         <section>
+            <header>
+                <h1>MineSweeper</h1>
+            </header>
 
+            {gameState === 0 && <CreateDifficulty />}
+            {(gameState > 0 && gameState < 4) && <CreateBoard />}
+            {(gameState > 0 && gameState < 4) && <CreateTimer />}
+            {gameState === 4}
         </section>
     )
 }
